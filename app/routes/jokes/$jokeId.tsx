c10eab06bc0318226ce416f539f4611137b42c4d
Tosini13 <jbartos13@gmail.com>
2022-07-21 21:40:40 +0200

    added unique joke

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,29 @@
+import type { Joke } from "@prisma/client";
+import { json, type LoaderFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+type LoaderData = {
+  joke: Joke;
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+  if (!joke) throw new Error("Joke not found");
+  const data: LoaderData = { joke };
+  return json(data);
+};
+
 export default function JokeRoute() {
+  const { joke } = useLoaderData<LoaderData>();
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{joke.createdAt}</p>
+      <p>{joke.content}</p>
+      <Link to=".">{joke.name} Permalink</Link>
     </div>
   );
 }
